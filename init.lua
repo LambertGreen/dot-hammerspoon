182b1f4ec3826e372b7b2d2a86ef6be153b700d9
Lambert Green <lambert_green@msn.com>
2021-06-05 15:12:29 -0700

    Disabling the use of ShiftToBrackets package.
    While the implementation works fine, I did not find myself making
    sufficient use of it, and instead being impacted heavily my spurious
    unintentional miss presses of the Shift key.

--- init.lua
+++ init.lua
@@ -6,7 +6,6 @@ hs.logger.defaultLogLevel = "info"
 require('HyperMode')
 require('ConfigWatcher')
 require('ControlTapToEscape')
-require('ShiftToBrackets')
 -- TODO Add keybinding in HyperMode for ShowKeys functionality
 require('ShowKeys')
 require('AppWatcher')
@@ -14,3 +13,10 @@ require('AppSwitcher')
 
 -- Alert whenever this config is loaded.
 hs.alert.show("Hammerspoon: config loaded")
+
+-- Disabled packages
+--
+-- No longer using ShiftToBrackets because it is better to learn to touch type the brackets after all.
+-- An issue with using it, was that often times I pressed Shift by mistake which resulted in spurious
+-- brackets being inputted.  This was especially annoying in password input fields.
+-- require('ShiftToBrackets')
