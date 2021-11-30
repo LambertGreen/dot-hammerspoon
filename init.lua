92cb382a7aa112a8e1d0e6c2eeb305dec896e2bc
Lambert Green <lambert_green@msn.com>
2021-11-29 17:04:03 -0800

    Disable Readline & UrlRouter
    Both are running into troubles. See comment blocks for details.

--- init.lua
+++ init.lua
@@ -14,8 +14,6 @@ require('ControlTapToEscape')
 require('ShowKeys')
 require('AppWatcher')
 require('AppSwitcher')
-require('Readline')
-require('UrlRouter')
 
 -- Alert whenever this config is loaded.
 hs.alert.show("Hammerspoon: config loaded")
@@ -26,3 +24,16 @@ hs.alert.show("Hammerspoon: config loaded")
 -- An issue with using it, was that often times I pressed Shift by mistake which resulted in spurious
 -- brackets being inputted.  This was especially annoying in password input fields.
 -- require('ShiftToBrackets')
+
+-- Disabling the use of UrlRouter for the following reasons:
+-- 1. It does not work when trying to use 2 Chrome profiles
+-- 2. It does not work when trying to use 2 browsers i.e. Chrome and Chrome Beta
+-- require('UrlRouter')
+
+-- Disabling the Readline since the only thing
+-- there is an attempt to add <ctrl-u> functionality,
+-- and we are running into issues with that.
+-- Fortunately macOS already has decent support
+-- for Readline like bindings.
+--
+-- require('Readline')
