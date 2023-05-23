244ce79c0b8325d8036052879544a8b154e01960
Lambert Green <lambert_green@msn.com>
2023-05-22 21:03:09 -0700

    Add 'cmd+q' override

--- application.lua
+++ application.lua
@@ -9,8 +9,16 @@ obj.appKill9 = function()
     hs.focus()
     local result = hs.dialog.blockAlert("Force kill application", "Are you sure?", "OK", "Cancel", "warning")
     if result == "OK" then
-      app:kill9()
+        app:kill9()
     end
 end
+
+-- Override 'CMD+q' to not silently kill application
+local applicationHotkey = hs.hotkey.new({ "cmd" }, "Q", function()
+    obj.appKill9()
+end)
+
+-- Enable the hotkey
+applicationHotkey:enable()
 
 return obj
