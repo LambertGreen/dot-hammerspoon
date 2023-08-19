ffd9f6642ca3b560adf104d4fdcdb0494a4c364d
Lambert Green <lambert_green@msn.com>
2023-08-19 12:42:11 -0700

    Disable CMD+Q overriding

--- application.lua
+++ application.lua
@@ -13,12 +13,16 @@ obj.appKill9 = function()
     end
 end
 
+-- TODO: Decide if this should be filtered to whether Contexts is running or not.
+-- Update 8/19/23: Disabling for now since closing apps via Contexts is pretty handy
+-- and this config is currently intercepting it.
+--
 -- Override 'CMD+q' to not silently kill application
-local applicationHotkey = hs.hotkey.new({ "cmd" }, "Q", function()
-    obj.appKill9()
-end)
+-- local applicationHotkey = hs.hotkey.new({ "cmd" }, "Q", function()
+--     obj.appKill9()
+-- end)
 
 -- Enable the hotkey
-applicationHotkey:enable()
+-- applicationHotkey:enable()
 
 return obj
